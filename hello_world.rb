b4e25c0871762eb91868f2a5caa869f319556fb0
Yatindra <yatindrarao.rao@gmail.com>
2017-02-22 12:31:23 +0530

    Added advice class for random advices on /advice ]

--- hello_world.rb
+++ hello_world.rb
@@ -1,5 +1,27 @@
+require_relative 'advice'
+
 class HelloWorld
   def call(env)
-    ['200', {'Content-Type' => 'text/plain'}, ['Hello World']]
+    case env['REQUEST_PATH']
+    when '/'
+      [
+        '200',
+        {"Content-Type" => "text/html"},
+        ["<h2>Hello World</h2>"]
+      ]
+    when '/advice'
+      piece_of_advice = Advice.new.generate  # random advice
+      [
+        '200',
+        {"Content-Type" => "text/html"},
+        ["<html><body><b><em>#{piece_of_advice}</em></b></body></html>"]
+      ]
+    else
+      [
+        '404',
+        {"Content-Type" => "text/html", "Content-Length" => '22'},
+        ["<h4>404 Not Found</h4>"]
+      ]
+    end
   end
 end
